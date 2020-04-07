fd8994eec3f2d7114919adcd658a17722825ad26
Ravi-Raushan <krravi015@gmail.com>
2020-04-07 19:01:38 +0530

    [Ravi Raushan]Added A function Which take 3 Integer number and return maximum

--- main/java/genericconcept/MaxNumberFinder.java
+++ main/java/genericconcept/MaxNumberFinder.java
@@ -1,5 +1,14 @@
 package genericconcept;
-// **********   WELCOME TO GENERIC CONCEPT PROGRAMS  ***********
-public class MaxNumberFinder {
 
+public class MaxNumberFinder {
+    public static Integer findingMaxNumber(Integer num1, Integer num2, Integer num3) {
+        Integer max = num1;
+        if (num2.compareTo(max) > 0) {
+            max = num2;
+        }
+        if (num3.compareTo(max) > 0) {
+            max = num3;
+        }
+        return max;
+    }
 }
