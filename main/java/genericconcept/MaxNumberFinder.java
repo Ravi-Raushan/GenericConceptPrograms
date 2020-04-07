e005c70bafcbfda139cd14f98a60cb93a76fb040
Ravi-Raushan <krravi015@gmail.com>
2020-04-07 21:35:12 +0530

    [Ravi Raushan]Added A generic function which take all three Integer, Float and String Class object and return max

--- main/java/genericconcept/MaxNumberFinder.java
+++ main/java/genericconcept/MaxNumberFinder.java
@@ -1,35 +1,15 @@
 package genericconcept;
 
 public class MaxNumberFinder {
-    public static Integer findingMaxNumber(Integer num1, Integer num2, Integer num3) {
-        Integer max = num1;
-        if (num2.compareTo(max) > 0) {
-            max = num2;
-        }
-        if (num3.compareTo(max) > 0) {
-            max = num3;
-        }
-        return max;
-    }
-    public static Float findingMaxNumber(Float num1, Float num2, Float num3) {
-        Float max = num1;
-        if (num2.compareTo(max) > 0) {
-            max = num2;
-        }
-        if (num3.compareTo(max) > 0) {
-            max = num3;
-        }
-        return max;
-    }
-    public static String findingMaxNumber(String str1, String str2, String str3) {
-        String max = str1;
-        if (str2.compareTo(max) > 0) {
-            max = str2;
-        }
-        if (str3.compareTo(max) > 0) {
-            max = str3;
-        }
-        return max;
-    }
 
+    public static <X extends Comparable> X findingMaxNumber(X a1, X a2, X a3) {
+        X max = a1;
+        if (a2.compareTo(max) > 0) {
+            max = a2;
+        }
+        if (a3.compareTo(max) > 0) {
+            max = a3;
+        }
+        return max;
+    }
 }
