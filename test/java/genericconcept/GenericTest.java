e1d22bdab730815de18f02e3591b978ba8f7a369
Ravi-Raushan <krravi015@gmail.com>
2020-04-07 19:02:25 +0530

    [Ravi Raushan]Added test cases to check max number Integer

--- test/java/genericconcept/GenericTest.java
+++ test/java/genericconcept/GenericTest.java
@@ -1,5 +1,22 @@
 package genericconcept;
 
-public class GenericTest {
+import org.junit.Assert;
+import org.junit.Test;
 
+public class GenericTest {
+    @Test
+    public void givenMaxNumberAtFirstPositionShouldReturnTheSameInteger() {
+        Integer maxNumber = MaxNumberFinder.findingMaxNumber(80, 29, 45);
+        Assert.assertEquals(Integer.valueOf(80), maxNumber);
+    }
+    @Test
+    public void givenMaxNumberAtSecondPositionShouldReturnTheSameInteger() {
+        Integer maxNumber = MaxNumberFinder.findingMaxNumber(29, 80, 45);
+        Assert.assertEquals(Integer.valueOf(80), maxNumber);
+    }
+    @Test
+    public void givenMaxNumberAtThirdPositionShouldReturnTheSameInteger() {
+        Integer maxNumber = MaxNumberFinder.findingMaxNumber(29, 45, 80);
+        Assert.assertEquals(Integer.valueOf(80), maxNumber);
+    }
 }
