5603b76489088ad2f2619b426b2862194cc8eb05
Ravi-Raushan <krravi015@gmail.com>
2020-04-07 19:37:29 +0530

    [Ravi Raushan]Added A function Which take 3 String and return maximum

--- main/java/genericconcept/MaxNumberFinder.java
+++ main/java/genericconcept/MaxNumberFinder.java
@@ -21,4 +21,15 @@ public class MaxNumberFinder {
         }
         return max;
     }
+    public static String findingMaxNumber(String str1, String str2, String str3) {
+        String max = str1;
+        if (str2.compareTo(max) > 0) {
+            max = str2;
+        }
+        if (str3.compareTo(max) > 0) {
+            max = str3;
+        }
+        return max;
+    }
+
 }
